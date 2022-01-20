8fabf7d2104238ee3895bd309430d5d89b0e7d75
Vadim88226 <kingdragon2108@gmail.com>
2022-01-20 13:58:09 +0300

    add env

---
+++ env.py
@@ -0,0 +1,10 @@
+aws_access_key_id="AKIASB66JFBS2OSVJAQD" 
+aws_secret_access_key= "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"
+
+# DB connection config
+ENDPOINT="textract.cz0hwy3wai9r.us-west-1.rds.amazonaws.com"
+PORT="3306"
+DB_USER="admin"
+REGION="us-west-1b"
+DBNAME="textract"
+PWD="adminadmin"
